03602d70c2f2769306871f0a79f6d102edb17b13
Ernst van Graan <ernst.van.graan@hetzner.co.za>
2015-10-17 16:41:44 +0200

    dss steps scaffolding

---
+++ features/step_definitions/dss_steps.rb
@@ -0,0 +1,39 @@
+Given(/^a service with DSS meta$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the service is selected for a query result$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the DSS is unavailable$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^a service query is performed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the service registry should not include the service in the result$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the DSS indicates the service should be included in the results$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the service registry should include the service in the result$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the DSS indicates the service should not be included in the results$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the DSS indicates the service is not known to it$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the DSS indicates an error in attempting to determine inclusion for the service$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
