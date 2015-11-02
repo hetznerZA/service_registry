e2247a8397b435d9a2f2f1e114ae676d8934ae8f
Ernst van Graan <ernst.van.graan@hetzner.co.za>
2015-11-02 14:17:39 +0200

    refactor wrap-up

--- lib/service_registry/test/production_service_component_orchestration_provider.rb
+++ lib/service_registry/test/production_service_component_orchestration_provider.rb
@@ -1,14 +1,17 @@
 module ServiceRegistry
   module Test
-    class ProductionServiceDefinitionOrchestrationProvider < BaseServiceDefinitionOrchestrationProvider
+    class ProductionServiceComponentOrchestrationProvider < BaseServiceComponentOrchestrationProvider
     end
   end
 end
 
-ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Registering service definitions", ServiceRegistry::Test::ProductionServiceDefinitionOrchestrationProvider)
-ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Deregistering a service definition", ServiceRegistry::Test::ProductionServiceDefinitionOrchestrationProvider)
-ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Retrieve a service definition for a service", ServiceRegistry::Test::ProductionServiceDefinitionOrchestrationProvider)
 
-ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Registering service definitions", ServiceRegistry::Test::ProductionServiceDefinitionOrchestrationProvider)
-ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Deregistering a service definition", ServiceRegistry::Test::ProductionServiceDefinitionOrchestrationProvider)
-ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Retrieve a service definition for a service", ServiceRegistry::Test::ProductionServiceDefinitionOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Listing service components", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Registering service components", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "De-registering a service component", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Configuring a URI for a service component", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Listing service components", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Registering service components", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "De-registering a service component", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Configuring a URI for a service component", ServiceRegistry::Test::ProductionServiceComponentOrchestrationProvider)
---
+++ lib/service_registry/test/production_service_orchestration_provider.rb
@@ -0,0 +1,16 @@
+module ServiceRegistry
+  module Test
+    class ProductionServiceOrchestrationProvider < BaseServiceOrchestrationProvider
+    end
+  end
+end
+
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "De-registering a service", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Registering services", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Searching for a service", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("tfa", "Map service components to services", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "De-registering a service", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Registering services", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Searching for a service", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
+ServiceRegistry::Test::OrchestrationProviderRegistry.instance.register("production", "Map service components to services", ServiceRegistry::Test::ProductionServiceOrchestrationProvider)
