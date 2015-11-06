6a44a838b737034ef1b5b51f9f764d4a55947109
Ernst van Graan <ernst.van.graan@hetzner.co.za>
2015-11-06 16:44:53 +0200

    juddi provider soaped up for service, business, and binding creation

--- tfa/try_client.rb
+++ tfa/try_client.rb
@@ -2,11 +2,43 @@ require '../lib/service_registry/providers/jsend_provider.rb'
 require '../lib/service_registry/providers/juddi_provider.rb'
 
 iut = ServiceRegistry::Providers::JUDDIProvider.new
-result = iut.register_service("temporary")
-services = iut.query_service_by_pattern('empo')['data']['services']
-services.each do |id, name|
-  @service = id if name == "temporary"
-end
-result = iut.deregister_service(@service)
+iut.authenticate('root', 'root')
 
-puts result
+test = 1
+
+if test == 1
+  result = iut.save_service('temporary', 'A temporary service', 'http://github.com/temp/one.wadl')
+byebug
+  result = iut.save_bindings('temporary', ['http://sc1.dev.auto-h/net/temporary', 'http://sc2.dev.auto-h/net/temporary'])
+  sleep 10
+  ##check in juddi
+
+  service = iut.get_service('temporary')
+
+  puts service
+
+  #services = iut.find_services['data']['services']
+  #puts services
+
+  services = iut.find_services('empo')['data']['services']
+  #puts services
+
+  result = iut.delete_service('temporary')
+  puts result
+end
+
+if test == 2
+  result = iut.save_business('billing')
+  sleep 10
+  ##check in juddi
+  result = iut.find_businesses
+  puts result
+
+  businesses = iut.find_businesses('billing')['data']['businesses']
+  businesses.each do |id, name|
+    @business = id if name == 'billing'
+  end
+
+  result = iut.delete_business(@business)
+  puts result
+end
