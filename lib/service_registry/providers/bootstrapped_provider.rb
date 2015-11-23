ed1a8cc47864c7d2abd46415cbbbf3d25ee14d8e
Ernst van Graan <ernst.van.graan@hetzner.co.za>
2015-11-23 14:33:03 +0200

    refactored bootstrap provider

--- lib/service_registry/providers/bootstrapped_provider.rb
+++ lib/service_registry/providers/bootstrapped_provider.rb
@@ -9,13 +9,7 @@ module ServiceRegistry
 
       def bootstrap(configuration_service_details, configuration_service)
         @available = false
-        return fail('invalid configuration service') if configuration_service_details.nil?
-        return fail('no configuration service') if configuration_service_details['CFGSRV_PROVIDER_ADDRESS'].nil?
-        return fail('no identifier') if configuration_service_details['CFGSRV_IDENTIFIER'].nil?
-        return fail('invalid identifier') if configuration_service_details['CFGSRV_IDENTIFIER'].strip == ""
-        @configuration = configuration_service.request_configuration
-        return fail('no configuration') if @configuration.nil?
-        return fail('invalid configuration') if @configuration.empty?
+        validation = configure_and_validate_bootstrap(configuration_service_details, configuration_service); return validation if validation['status'] == 'fail'
         @available = true
         return success(['configuration valid', 'valid identifier']) if @configuration
       rescue => ex
@@ -24,6 +18,29 @@ module ServiceRegistry
 
       def configuration
         @configuration
+      end
+
+      private
+
+      def configure_and_validate_bootstrap(configuration_service_details, configuration_service)
+        validation = validate_configuration_service_details(configuration_service_details); return validation if validation['status'] == 'fail'
+        @configuration = configuration_service.request_configuration
+        validation = validate_configuration_retrieved; return validation if validation['status'] == 'fail'
+        success        
+      end
+
+      def validate_configuration_service_details(configuration_service_details)
+        return fail('invalid configuration service') if configuration_service_details.nil?
+        return fail('no configuration service') if configuration_service_details['CFGSRV_PROVIDER_ADDRESS'].nil?
+        return fail('no identifier') if configuration_service_details['CFGSRV_IDENTIFIER'].nil?
+        return fail('invalid identifier') if configuration_service_details['CFGSRV_IDENTIFIER'].strip == ""
+        success
+      end
+
+      def validate_configuration_retrieved
+        return fail('no configuration') if @configuration.nil?
+        return fail('invalid configuration') if @configuration.empty?
+        success
       end
     end
   end
