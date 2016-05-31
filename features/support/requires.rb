e4bd88b8e8548e8efad78b0d1011c36650432550
Ernst Van Graan <ernst.van.graan@hetzner.co.za>
2016-05-31 21:28:25 +0200

    Jenkins support

--- features/support/requires.rb
+++ features/support/requires.rb
@@ -7,6 +7,12 @@ class Requires
 	  require "providers/juddi_provider"
 	  require "test/orchestration_provider_registry"
 	  require "test/base_orchestration_provider"
+          require "test/base_domain_perspective_orchestration_provider"
+          require "test/contacts_orchestration_provider"
+          require "test/base_service_orchestration_provider"
+          require "test/base_meta_orchestrator"
+          require "test/base_dss_orchestration_provider.rb"
+          require "test/base_service_component_orchestration_provider.rb"
 	  Dir.glob(File.expand_path("../../../lib/service_registry/test/**/*.rb", __FILE__), &method(:require))
 	end
 end
